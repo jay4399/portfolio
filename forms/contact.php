88b3e5c9acaa8d388a97a71297b1713caef8180d
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-09-15 15:53:35 +0400

    Worked on Favicon

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,7 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'jay4399@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
