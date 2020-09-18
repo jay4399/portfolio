8d557cdd2ed0da56bca315074c5c844e197f340d
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-09-18 17:07:50 +0400

    Updates

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+     if(isset($_POST['submit']))
+     {
+       echo 'IT WORKED!';
+
+      //  $name = $_POST['name'];
+      //  $mailFrom = $_POST['email'];
+      //  $subject = $_POST['subject'];
+      //  $message = $_POST['message'];
+
+      //  $mailTo = "jay4399@gmail.com";
+      //  $headers = "From: ".$mailFrom;
+      //  $txt = "You have recieved an email from".$name.".\n\n".$message;
+
+      //  mail($mailTo, $subject, $txt, $headers);
+      //  header("Location: index.php?mailsend");
+
+     }
+?>
