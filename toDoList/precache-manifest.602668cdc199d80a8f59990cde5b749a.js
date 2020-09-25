51ca1a037491c545e7224ccaeca19791e575958e
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-09-25 15:41:00 +0400

    Fixed React to do list?

---
+++ toDoList/precache-manifest.602668cdc199d80a8f59990cde5b749a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3dd5e587f6bbe223077e747d92a414",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "016ab8fc54607d7d3d9e",
+    "url": "/toDoList/static/css/main.e0b7869f.chunk.css"
+  },
+  {
+    "revision": "6d7e3632e7aea1f8a11f",
+    "url": "/toDoList/static/js/2.dcc39864.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/toDoList/static/js/2.dcc39864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016ab8fc54607d7d3d9e",
+    "url": "/toDoList/static/js/main.2ebe8bfa.chunk.js"
+  },
+  {
+    "revision": "ce7419d06ee66e234317",
+    "url": "/toDoList/static/js/runtime-main.575a5a11.js"
+  }
+]);
