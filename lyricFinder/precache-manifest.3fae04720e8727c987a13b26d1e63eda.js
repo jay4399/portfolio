e103534f0d7381e27d38bdb5da83b4d9ac667194
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-10-03 02:09:02 +0400

    Added movieBrowser and lyricFinder

---
+++ lyricFinder/precache-manifest.3fae04720e8727c987a13b26d1e63eda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3197e93d408a4662db8fd47cb739a12",
+    "url": "/LyricFinder/index.html"
+  },
+  {
+    "revision": "46fb9bd0b1b190edb2b3",
+    "url": "/LyricFinder/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "4e7de6b8dcd3396fb50e",
+    "url": "/LyricFinder/static/js/2.dd9a355b.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/LyricFinder/static/js/2.dd9a355b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46fb9bd0b1b190edb2b3",
+    "url": "/LyricFinder/static/js/main.4156ac1e.chunk.js"
+  },
+  {
+    "revision": "5e062a2b017f20740e95",
+    "url": "/LyricFinder/static/js/runtime-main.94dc8201.js"
+  }
+]);
---
+++ movieBrowser/precache-manifest.05f255f2ffcd65c2f5f67ab0ee45e163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78daa38fc787f6b467218b8266ca696e",
+    "url": "/movieBrowser/index.html"
+  },
+  {
+    "revision": "c6a9b89b608f98debd90",
+    "url": "/movieBrowser/static/css/main.ad92fb3f.chunk.css"
+  },
+  {
+    "revision": "74e44828038c2ee07b04",
+    "url": "/movieBrowser/static/js/2.19ec3224.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieBrowser/static/js/2.19ec3224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a9b89b608f98debd90",
+    "url": "/movieBrowser/static/js/main.fc849f04.chunk.js"
+  },
+  {
+    "revision": "d3b71f7ac43b4ee13ca1",
+    "url": "/movieBrowser/static/js/runtime-main.0ebd3536.js"
+  }
+]);
