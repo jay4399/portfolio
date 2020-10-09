c2be5266e9435105a88dbe81cdc5409bf2618bce
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-10-09 20:28:21 +0400

    Updates

---
+++ Projects/covidTracker/precache-manifest.a6aa2c93326f9fc22ed807b68e16872a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed5431f9d4dfa25ef166e84c5a722d3",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "9de748ffadca8c752448",
+    "url": "/covidTracker/static/css/main.9060726e.chunk.css"
+  },
+  {
+    "revision": "37885d025ffe6e53656e",
+    "url": "/covidTracker/static/js/2.a9dd99cb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTracker/static/js/2.a9dd99cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de748ffadca8c752448",
+    "url": "/covidTracker/static/js/main.f1ec6161.chunk.js"
+  },
+  {
+    "revision": "4b5157204f46eb09a506",
+    "url": "/covidTracker/static/js/runtime-main.6c775530.js"
+  }
+]);
