2a34e5fe5db729b4edb9f0c20c4ca57972122962
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-10-14 12:56:56 +0400

    Updates

---
+++ Projects/googleClone/precache-manifest.955d3d26861e664fc12f647551c6fd58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd6706f12ddfa1513985b7cd0c53611",
+    "url": "/googleClone/index.html"
+  },
+  {
+    "revision": "cc58ed951c77d0b57aea",
+    "url": "/googleClone/static/css/main.43c522d3.chunk.css"
+  },
+  {
+    "revision": "044199f82b8daa45ca3a",
+    "url": "/googleClone/static/js/2.1578dff8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/googleClone/static/js/2.1578dff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc58ed951c77d0b57aea",
+    "url": "/googleClone/static/js/main.0126c660.chunk.js"
+  },
+  {
+    "revision": "850de36cbe7a049fc94e",
+    "url": "/googleClone/static/js/runtime-main.3c5c21b6.js"
+  }
+]);
